ec841d2c782f0aa636a2b29a8f80989935ef0c58
Kalen <kalenhoneyfield@gmail.com>
2020-07-22 06:31:39 -0400

    added a filter to pet

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <h1>{{ animal.name }} ({{ $route.params.species | removeS }})</h1>
     <p>Age: {{ animal.age }} years old</p>
     <p>Bread: {{ animal.breed }}</p>
   </div>
@@ -21,6 +21,11 @@ export default {
   },
   computed: {
     ...mapState(['cats', 'dogs']),
+  },
+  filters: {
+    removeS(word) {
+      return word.replace(/(\w+)(s)$/i, '$1');
+    },
   },
 };
 </script>
