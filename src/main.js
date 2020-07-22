4271d797405d551c2ed99b653371ccace4e28450
Kalen <kalenhoneyfield@gmail.com>
2020-07-22 07:47:55 -0400

    added sass

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import './assets/sass/index.sass';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home-view-container">
     <h1>Pets</h1>
     {{ animalsCount }}
     {{ getAllCats.length }}
